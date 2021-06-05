e2ae17389fe8df59c8d0d7c23c8d270eb9f9ce1c
Steve Richter <steve@nerdbrain.com>
2021-06-05 02:00:20 -0400

    Fallback to $PORT for Heroku compatibility

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from os import getenv
 
 if __name__ == '__main__':
     host = getenv('ASKS_HOST', '0.0.0.0')
-    port = int(getenv('ASKS_PORT', 4545))
+    port = int(getenv('ASKS_PORT', getenv('PORT', 443)))
     debug = getenv('ASKS_DEBUG', 'False') == 'True'
 
     app.run(host=host, port=port, debug=debug)
