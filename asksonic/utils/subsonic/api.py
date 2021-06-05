30ec37e8fb5e4b4e293cc183ca04a14c754c6e70
Steve Richter <steve@nerdbrain.com>
2021-06-05 01:24:47 -0400

    Configure extra query parameter to send with all Subsonic requests

--- asksonic/utils/subsonic/api.py
+++ asksonic/utils/subsonic/api.py
@@ -7,9 +7,17 @@ from asksonic.utils.subsonic.track import Track
 class Subsonic(Connection):
     def __init__(
         self,
-        baseUrl: str, username: str, password: str, port: int
+        baseUrl: str, username: str, password: str, port: int,
+        extra_param: tuple
     ) -> None:
         super().__init__(baseUrl, username, password, port)
+        self._extra_param = extra_param
+
+    def _getBaseQdict(self) -> dict[str, str]:
+        qdict = super()._getBaseQdict()
+        if self._extra_param:
+            qdict.update([self._extra_param])
+        return qdict
 
     def request_url(self, request: Request) -> str:
         url = request.get_full_url()
@@ -30,9 +38,18 @@ subsonic_pass = getenv('ASKS_SUBSONIC_PASS', '')
 if any(x == '' for x in [subsonic_url, subsonic_user, subsonic_pass]):
     raise RuntimeError('Subsonic login information is missing from .env')
 
+try:
+    extra_param = tuple(getenv('ASKS_EXTRA_PARAM').split(',', 1))
+    if len(extra_param) != 2:
+        raise RuntimeError('The format of ASKS_EXTRA_PARAM is: "key,value"')
+except AttributeError:
+    extra_param = ()
+
+
 subsonic = Subsonic(
     subsonic_url,
     subsonic_user,
     subsonic_pass,
     int(getenv('ASKS_SUBSONIC_PORT', 443)),
+    extra_param
 )
