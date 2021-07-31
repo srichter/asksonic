b07200352ec9cc7e66792d203815da9617c21b9b
Steve Richter <steve@nerdbra.in>
2021-07-31 03:52:30 -0400

    Add "play artist" and "play album" Intents

--- asksonic/intents/navigation.py
+++ asksonic/intents/navigation.py
@@ -1,3 +1,5 @@
+from typing import Optional, Union
+from flask_ask.models import statement
 from asksonic.utils.response import play_track_response
 from flask import render_template
 from flask_ask import question, audio
@@ -23,6 +25,37 @@ def play_random_tracks() -> audio:
     tracks = subsonic.random_tracks(tracks_count)
     track = queue.reset(tracks)
     return play_track_response(track, render_template('playing_library'))
+
+
+@ask.intent('AskSonicPlayArtistIntent')
+def play_artist(artist: str) -> Union[audio, statement]:
+    log(f'Play Artist: {artist}')
+    tracks = subsonic.artist_tracks(artist, tracks_count)
+    if tracks:
+        track = queue.reset(tracks)
+        return play_track_response(
+            track,
+            render_template('playing_artist', artist=track.artist)
+        )
+    return statement(render_template('artist_not_found', artist=artist))
+
+
+@ask.intent('AskSonicPlayAlbumIntent')
+def play_album(album: str, artist: Optional[str]) -> Union[audio, statement]:
+    log(f'Play Album: {artist} - {album}')
+    tracks = subsonic.album_tracks(album, artist)
+    if tracks:
+        track = queue.reset(tracks)
+        return play_track_response(
+            track,
+            render_template(
+                'playing_album',
+                album=track.album, artist=track.artist
+            )
+        )
+    return statement(
+        render_template('album_not_found', album=album, artist=artist)
+    )
 
 
 def log(msg: str) -> None:
--- asksonic/utils/subsonic/api.py
+++ asksonic/utils/subsonic/api.py
@@ -2,6 +2,7 @@ from typing import Optional
 from urllib.parse import quote_plus
 from libsonic import Connection
 from urllib.request import Request
+from random import shuffle
 from .track import Track
 
 
@@ -26,8 +27,86 @@ class Subsonic(Connection):
             query += f'&asksonic-secret={quote_plus(self._extra_secret)}'
         return f'{url}?{query}'
 
-    def random_tracks(self, count: int) -> list[Track]:
+    def search(self, query: str, search_options: dict = {}) -> dict:
+        default_options = {
+            'artistCount': 0, 'artistOffset': 0, 'albumCount': 0,
+            'albumOffset': 0, 'songCount': 0, 'songOffset': 0,
+            'musicFolderId': None
+        }
+        options = default_options | search_options
+        return self.search3(query=query, **options)
+
+    def _find_x(self, search_type: str, query: str) -> Optional[dict]:
+        if search_type not in ['artist', 'album']:
+            raise ValueError('search_type must be one of: artist, album')
+        result = self.search(
+            query=query,
+            search_options={f'{search_type}Count': 10}
+        )
+        if search_type not in result['searchResult3']:
+            return None
+        try:
+            found = next(
+                x for x in result['searchResult3'][search_type]
+                if x['name'].lower() == query.lower()
+            )
+        except StopIteration:
+            found = result['searchResult3'][search_type][0]
+        return found
+
+    def find_artist(self, query: str) -> Optional[dict]:
+        return self._find_x('artist', query)
+
+    def find_album(
+        self,
+        query: str, artist_id: Optional[str]
+    ) -> Optional[dict]:
+        if artist_id:
+            albums = self.getArtist(artist_id)
+            albums = albums['artist']['album']
+            try:
+                return next(
+                    x for x in albums
+                    if x['name'].lower() == query.lower()
+                )
+            except StopIteration:
+                return None
+        return self._find_x('album', query)
+
+    def random_tracks(self, count: int = 50) -> list[Track]:
         tracks = self.getRandomSongs(count)
         tracks = tracks['randomSongs']['song']
+        tracks = [Track(**track) for track in tracks]
+        return tracks
+
+    def artist_tracks(self, artist: str, count: int = 50) -> list[Track]:
+        found_artist = self.find_artist(artist)
+        if not found_artist:
+            return []
+        albums = self.getArtist(found_artist['id'])
+        albums = albums['artist']['album']
+        shuffle(albums)
+        tracks = []
+        for album in albums:
+            songs = self.getAlbum(album['id'])
+            songs = songs['album']['song']
+            shuffle(songs)
+            tracks.extend([Track(**track) for track in songs])
+            if len(tracks) >= count:
+                tracks = tracks[:count]
+                break
+        shuffle(tracks)
+        return tracks
+
+    def album_tracks(self, album: str, artist: Optional[str]) -> list[Track]:
+        if artist:
+            found_artist = self.find_artist(artist)
+            if found_artist:
+                artist = found_artist['id']
+        found_album = self.find_album(album, artist)
+        if not found_album:
+            return []
+        tracks = self.getAlbum(found_album['id'])
+        tracks = tracks['album']['song']
         tracks = [Track(**track) for track in tracks]
         return tracks
